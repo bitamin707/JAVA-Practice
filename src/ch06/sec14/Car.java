8227384a769d099fafe20761beb8600f30f7f8c4
WonGyu <bitamin707@naver.com>
2022-11-01 19:28:37 +0900

    sec14 Getter and Setter

---
+++ src/ch06/sec14/Car.java
@@ -0,0 +1,33 @@
+package ch06.sec14;
+
+public class Car {
+	//필드 선언
+	private int speed;
+	private boolean stop;
+	
+	//speed 필드 Getter and Setter
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		if(speed < 0) {
+			this.speed = 0;
+		}
+		else {
+			this.speed = speed;
+		}
+	}
+	
+	//stop 필드 Getter and Setter
+	public boolean isStop() {
+		return stop;
+	}
+	public void setStop(boolean stop) {
+		this.stop = stop;
+		if(stop) {
+			this.speed = 0;
+		}
+	}
+	
+	
+}
