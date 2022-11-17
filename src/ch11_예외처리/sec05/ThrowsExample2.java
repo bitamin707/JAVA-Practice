ac6cdc60d48a03bb55dc7dd5f7453c4b295dea34
WonGyu <bitamin707@naver.com>
2022-11-17 17:07:47 +0900

    sec05 예외 떠넘기기

---
+++ src/ch11_예외처리/sec05/ThrowsExample2.java
@@ -0,0 +1,15 @@
+package ch11_예외처리.sec05;
+
+public class ThrowsExample2 {
+
+	public static void main(String[] args) throws Exception {
+
+		findClass();
+		
+	}
+	
+	public static void findClass() throws ClassNotFoundException {
+		Class.forName("java.lang.String2");
+	}
+
+}
